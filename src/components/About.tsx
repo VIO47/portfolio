f51259e4ccb750d3051fc3db63e8b0f4f714b005
vmacsim <v.m.macsim@student.tudelft.nl>
2025-03-02 21:56:46 +0100

    Add curly footer

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,21 +15,21 @@ const About = () => {
         <h2>ABOUT ME 👋</h2>
         <div className="about__text-1__grid half-center-grid">
           <div className="about__text-1__1">
-            <h4>
+            <p>
               Hello! My name is Violeta and I am a young and enthusiastic software
               developer eager to broaden my horizons. I'm passionate about the
               entire development process—whether it's diving into market and
               user research, coming up with designs, or bringing ideas to life
               through implementation.
-            </h4>
+            </p>
           </div>
           <div className="about__text-1__2">
-            <h4>
+            <p>
               I am always eager to learn and improve my skills. In my spare
               time, I play around in Figma and read about UX principles and best
               practices, expanding my knowledge of user-centered design and how
               it influences digital experiences.
-            </h4>
+            </p>
           </div>
         </div>
       </div>
@@ -38,19 +38,19 @@ const About = () => {
         <div className="about__text-2__grid">
           <div className="about__text-2__1">
             <h2>BEYOND MY 9 TO 5</h2>
-            <h4>
+            <p>
               When I'm not in front of my computer, you'll find me enjoying a
               cup of coffee and a good book. Aside from my self-development
               books, I'm always on the hunt for good thrillers or crime novels
               to read. I also capture a lot of photos of my culinary
               experiments!
-            </h4>
-            <h4>
+            </p>
+            <p>
               Traveling is a huge passion of mine, and I enjoy discovering new
               places. My Google Maps is constantly filled with new pinpoints to
               see or try, even in my hometown. Step by step, I cross off my bucket
               list, collecting unforgettable experiences along the way.
-            </h4>
+            </p>
           </div>
           <div className="about__text-2__2">
             <ImageCarousel height={440} images={images} time={6000} />
@@ -63,19 +63,19 @@ const About = () => {
         <div className="half-center-grid">
           <div className="about__text-3__masters">
             <h3 className="title">🎓 Msc Computer Science | TU Delft</h3>
-            <h4>
+            <p>
               I'm pursuing a Master's degree in Software Engineering and
               Information Management, complemented by additional courses such as
               System Design and Research Ethics. Through team projects and
               research, I gained hands-on experience in both technical and
               user-focused topics, including the use of LLMs in software
               engineering and quality assessment of reading platform UIs.
-            </h4>
+            </p>
           </div>
 
           <div className="about__text-3__bachelors">
             <h3 className="title">🎓 Bsc Computer Science & Engineering | TU Delft</h3>
-            <h4>
+            <p>
               I completed my Bachelor's degree with a specialization in Data,
               where I studied core computer science and engineering concepts. My
               curriculum covered core programming & software engineering
@@ -83,12 +83,12 @@ const About = () => {
               gained hands-on experience in team-based software development
               through projects introducing me to the industry setting, while
               also exploring academic research through my final thesis.
-            </h4>
+            </p>
           </div>
 
           <div className="about__text-3__minor">
             <h3 className="title">📝 Minor Project Management | TU Delft</h3>
-            <h4>
+            <p>
               For five months, I collaborated with students from diverse
               backgrounds on a university project for expanding the campus
               grounds. The minor covered all stages of a complex project,
@@ -96,17 +96,17 @@ const About = () => {
               management, finance, and crisis management. It was complemented by
               courses in project management, law, and finance, emphasizing
               adaptability to project requirements.
-            </h4>
+            </p>
           </div>
 
           <div className="about__text-3__capm">
             <h3 className="subtitle">👩🏼‍🎓 CAPM Diploma | PMI The Netherlands</h3>
-            <h4>
+            <p>
               Earned an introductory Project Management diploma through
               self-study and a few guided lectures by CAPM The Netherlands. It
               covered key topics like project management basics, scheduling,
               business analysis, collaboration, and team dynamics.
-            </h4>
+            </p>
           </div>
         </div>
       </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,26 +1,60 @@
 import { FaLinkedin, FaSquareGithub } from "react-icons/fa6";
 import "@style/Footer.scss";
+import { useState, useEffect } from "react";
 
 const Footer = () => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 690);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setIsMobile(window.innerWidth < 690);
+      };
+  
+      window.addEventListener("resize", handleResize);
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }, []);
   return (
     <footer>
-      <div className="footer__1">
-        <h3>MACSIM VIOLETA-MARA</h3>
-        <p>Full-stack Developer | UX Enthusiast | Associate Project Manager</p>
-      </div>
-      <div className="footer__2">
-        <h3>🍵Let's connect!</h3>
-        <div className="footer__links">
-          <a
-            href="https://www.linkedin.com/in/violeta-mara-macsim-941931215/"
-            target="_blank"
-            className="icon"
-          >
-            <FaLinkedin />
-          </a>
-          <a href="https://github.com/VIO47" target="_blank" className="icon">
-            <FaSquareGithub />
-          </a>
+      {isMobile ? (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 475">
+          <path
+            fill="#D96A7F"
+            fillOpacity="1"
+            d="M0,190L34.3,196.3C68.6,202,137,214,206,201.7C274.3,190,343,152,411,126.7C480,101,549,87.5,617,74C685.7,61.5,754,49.5,823,37.5C891.4,25.5,960,13.5,1029,59C1097.1,104.5,1166,209.5,1234,241.7C1302.9,274,1371,234,1406,214L1440,190L1440,475L0,475Z"
+          ></path>
+        </svg>
+      ) : (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 260">
+          <path
+            fill="#D96A7F"
+            fillOpacity="1"
+            d="M0,160L34.3,165.3C68.6,171,137,181,206,170.7C274.3,160,343,128,411,106.7C480,85,549,75,617,64C685.7,53,754,43,823,32C891.4,21,960,11,1029,48C1097.1,85,1166,171,1234,197.3C1302.9,224,1371,192,1406,176L1440,160L1440,260L1405.7,260C1371.4,260,1303,260,1234,260C1165.7,260,1097,260,1029,260C960,260,891,260,823,260C754.3,260,686,260,617,260C548.6,260,480,260,411,260C342.9,260,274,260,206,260C137.1,260,69,260,34,260L0,260Z"
+          ></path>
+        </svg>
+      )}
+      <div className="footer__content">
+        <div className="footer__1">
+          <h3>MACSIM VIOLETA-MARA</h3>
+          <p>
+            Full-stack Developer | UX Enthusiast | Associate Project Manager
+          </p>
+        </div>
+        <div className="footer__2">
+          <h3>🍵Let's connect!</h3>
+          <div className="footer__links">
+            <a
+              href="https://www.linkedin.com/in/violeta-mara-macsim-941931215/"
+              target="_blank"
+              className="icon"
+            >
+              <FaLinkedin />
+            </a>
+            <a href="https://github.com/VIO47" target="_blank" className="icon">
+              <FaSquareGithub />
+            </a>
+          </div>
         </div>
       </div>
     </footer>
